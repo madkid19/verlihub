8136a0891205bddee2e821484bfbc761a8c32c52
netcelli_gentoo <netcelli_gentoo@users.sourceforge.net>
2009-04-01 12:39:56 +0200

    Removed BotINFO that is client side extention

--- trunk/verlihub/src/cdcproto.cpp
+++ trunk/verlihub/src/cdcproto.cpp
@@ -1366,7 +1366,7 @@ int cDCProto::DC_OpForceMove(cMessageDC * msg, cConnDC * conn)
 /** Extended support features */
 int cDCProto::DCE_Supports(cMessageDC * msg, cConnDC * conn)
 {
-	string omsg("$Supports OpPlus NoGetINFO NoHello UserIP2 HubINFO BotINFO");
+	string omsg("$Supports OpPlus NoGetINFO NoHello UserIP2 HubINFO");
 	istringstream is(msg->mStr);
 
 	string feature;
