56ac0a3a3ee6cb606319a242919729d66ddd834d
RoLex <fakeclock@users.sourceforge.net>
2011-10-18 07:18:21 +0200

    Performance: Don't actually generate a key when drop_invalid_key is disabled

--- src/cdcproto.cpp
+++ src/cdcproto.cpp
@@ -239,23 +239,26 @@ int cDCProto::DC_ValidateNick(cMessageDC *msg, cConnDC *conn)
 
 int cDCProto::DC_Key(cMessageDC * msg, cConnDC * conn)
 {
-	if(msg->SplitChunks()) return -1;
-	// Key already sent
-	if(conn->GetLSFlag(eLS_KEYOK)) {
+	if (msg->SplitChunks()) return -1;
+
+	if (conn->GetLSFlag(eLS_KEYOK)) { // key already sent
 		string omsg = _("Invalid login sequence. Key already sent");
-		if(conn->Log(1)) conn->LogStream() << omsg << endl;
-		mS->ConnCloseMsg(conn,omsg,1000, eCR_LOGIN_ERR);
+		if (conn->Log(1)) conn->LogStream() << omsg << endl;
+		mS->ConnCloseMsg(conn, omsg, 1000, eCR_LOGIN_ERR);
 		return -1;
 	}
-	string key, lock("EXTENDEDPROTOCOL_" PACKAGE);
-	Lock2Key(lock, key);
-	if(key != msg->ChunkString(1)) {
-		if(conn->Log(1)) conn->LogStream() << "Invalid key" << endl;
-		if(mS->mC.drop_invalid_key) {
-			mS->ConnCloseMsg(conn, _("Your client provided an invalid key"),1000, eCR_INVALID_KEY);
+
+	if (mS->mC.drop_invalid_key) {
+		string key, lock("EXTENDEDPROTOCOL_" PACKAGE);
+		Lock2Key(lock, key);
+
+		if (key != msg->ChunkString(1)) {
+			if (conn->Log(1)) conn->LogStream() << "Invalid key" << endl;
+			mS->ConnCloseMsg(conn, _("Your client provided an invalid key"), 1000, eCR_INVALID_KEY);
 			return -1;
 		}
 	}
+
 	conn->SetLSFlag(eLS_KEYOK);
 	conn->ClearTimeOut(eTO_KEY);
 	conn->SetTimeOut(eTO_VALNICK, mS->mC.timeout_length[eTO_VALNICK], mS->mTime);
