4dac5900b1e709f152c4b15f2d3fb8dd071d71a1
Alexander Zenkov <webmaster@feardc.net>
2013-03-22 17:09:26 +0100

    Update callbacks.h

--- plugins/lua/callbacks.h
+++ plugins/lua/callbacks.h
@@ -2,7 +2,7 @@
 *   Original Author: Daniel Muller (dan at verliba dot cz)                *
 *                    Janos Horvath (bourne at freemail dot hu) 2004-05    *
 *                                                                         *
-*   Copyright (C) 2006-2011 by Verlihub Project                           *
+*   Copyright (C) 2006-2013 by Verlihub Project                           *
 *   devs at verlihub-project dot org                                      *
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -30,59 +30,55 @@ extern "C"
 #include "cpilua.h"
 #include "cluainterpreter.h"
 namespace nVerliHub {
-	int _SendToUser(lua_State *);
-	int _SendToClass(lua_State *);
-	int _SendToAll(lua_State *);
-	int _SendPMToAll(lua_State *);
-	int _Disconnect(lua_State *);
-	int _StopHub(lua_State *);
-	int _GetMyINFO(lua_State *);
-	int _GetUserCC(lua_State *);
-	int _GetIPCC(lua_State *);
-	int _GetIPCN(lua_State *);
-	int _GetVHCfgDir(lua_State *);
+	int _SendToUser(lua_State *L);
+	int _SendToClass(lua_State *L);
+	int _SendToAll(lua_State *L);
+	int _SendPMToAll(lua_State *L);
+	int _Disconnect(lua_State *L);
+	int _StopHub(lua_State *L);
+	int _GetMyINFO(lua_State *L);
+	int _GetUserCC(lua_State *L);
+	int _GetIPCC(lua_State *L);
+	int _GetIPCN(lua_State *L);
+	int _GetVHCfgDir(lua_State *L);
 	int _GetUpTime(lua_State *L);
-	int _RegBot(lua_State *);
-	int _UnRegBot(lua_State *);
+	int _RegBot(lua_State *L);
+	int _UnRegBot(lua_State *L);
 	int _EditBot(lua_State *L);
 	int _IsBot(lua_State *L);
-	int _GetHubIp(lua_State *);
-	int _GetHubSecAlias(lua_State *);
-	int _SetConfig(lua_State *);
-	int _GetConfig(lua_State *);
-	int _GetUserClass(lua_State *);
-	int _GetUserHost(lua_State *);
-	int _GetUserIP(lua_State *);
+	int _GetHubIp(lua_State *L);
+	int _GetHubSecAlias(lua_State *L);
+	int _SetConfig(lua_State *L);
+	int _GetConfig(lua_State *L);
+	int _GetUserClass(lua_State *L);
+	int _GetUserHost(lua_State *L);
+	int _GetUserIP(lua_State *L);
 	int _IsUserOnline(lua_State *L);
 	int _InUserSupports(lua_State *L);
-	int _GetNickList(lua_State *);
-	int _GetOPList(lua_State *);
-	int _GetBotList(lua_State *);
-	int _GetBots(lua_State *);
-	int _Ban(lua_State *);
-	int _KickUser(lua_State *);
-	int _ReportUser(lua_State *);
-	int _SendToOpChat(lua_State *);
-	int _ParseCommand(lua_State *);
-
-	int _SQLQuery(lua_State *);
-	int _SQLFetch(lua_State *);
-	int _SQLFree(lua_State *);
-
+	int _GetNickList(lua_State *L);
+	int _GetOPList(lua_State *L);
+	int _GetBotList(lua_State *L);
+	int _GetLuaBots(lua_State *L);
+	int _Ban(lua_State *L);
+	int _KickUser(lua_State *L);
+	int _ReportUser(lua_State *L);
+	int _SendToOpChat(lua_State *L);
+	int _ParseCommand(lua_State *L);
+	int _SQLQuery(lua_State *L);
+	int _SQLFetch(lua_State *L);
+	int _SQLFree(lua_State *L);
 	int _GetUsersCount(lua_State *L);
-	int _GetTotalShareSize(lua_State *);
-
-	int _GetTempRights(lua_State *);
-	int _SetTempRights(lua_State *);
-	int _AddRegUser(lua_State *);
-	int _DelRegUser(lua_State *);
+	int _GetTotalShareSize(lua_State *L);
+	int _GetTempRights(lua_State *L);
+	int _SetTempRights(lua_State *L);
+	int _AddRegUser(lua_State *L);
+	int _DelRegUser(lua_State *L);
 	int _GetTopic(lua_State *L);
 	int _SetTopic(lua_State *L);
+	int _ScriptCommand(lua_State *L);
 
-	int _ScriptCommand(lua_State *);
-
-	nLuaPlugin::cLuaInterpreter *FindLua(lua_State *lua_state);
-	void luaerror(lua_State *, const char *);
+	nLuaPlugin::cLuaInterpreter *FindLua(lua_State *L);
+	void luaerror(lua_State *L, const char *errstr);
 }; // namespace nVerliHub
 
 #endif
