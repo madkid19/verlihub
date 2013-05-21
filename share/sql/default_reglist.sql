a67f15dc1a730cf6ccfd0b1e0d07abef0a025c0f
Alexander Zenkov <webmaster@feardc.net>
2013-05-21 05:16:55 +0300

    Added TEPinger instead of FearListPinger

--- share/sql/default_reglist.sql
+++ share/sql/default_reglist.sql
@@ -1,6 +1,6 @@
-INSERT IGNORE INTO reglist (nick,class,reg_date,reg_op,pwd_change,note_op) VALUES
-	('pinger', -1,UNIX_TIMESTAMP(NOW()),'installation',0,'generic pinger nick'),
-	('dchublist',-1,UNIX_TIMESTAMP(NOW()),'installation',0,'dchublist.com pinger'),
-	('FearListPinger',-1,UNIX_TIMESTAMP(NOW()),'installation',0,'feardc hublist pinger'),
-	('publichublist',-1,UNIX_TIMESTAMP(NOW()),'installation',0,'publichublist pinger'),
-	('dchublist_ru',-1,UNIX_TIMESTAMP(NOW()),'installation',0,'dchublist.ru pinger')
+INSERT IGNORE INTO reglist (nick, class, reg_date, reg_op, pwd_change, note_op) VALUES
+	('pinger', -1, UNIX_TIMESTAMP(NOW()), 'installation', 0, 'Generic pinger nick'),
+	('dchublist', -1, UNIX_TIMESTAMP(NOW()), 'installation', 0, 'DCHublist.com'),
+	('TEPinger', -1, UNIX_TIMESTAMP(NOW()), 'installation', 0, 'Team Elite Hublist'),
+	('publichublist', -1, UNIX_TIMESTAMP(NOW()), 'installation', 0, 'PublicHublist'),
+	('dchublist_ru', -1, UNIX_TIMESTAMP(NOW()), 'installation', 0, 'DCHublist.ru')
